de158a5802be5ff54dbbdd5651cc293d8b50508d
MartinSilvaJose <118887247+MartinSilvaJose@users.noreply.github.com>
2024-06-03 10:20:22 +0200

    Update page title and add CSS file import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useEffect } from "react"
+import './App.css'
 
 function App() {
   const [characters, setCharacters] = useState([]); // Array de personajes
